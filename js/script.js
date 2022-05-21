0c9bb29bc916b1013d7cc97bcb2282de650b86a0
eyekavya <kavyasrivastava7830@gmail.com>
2022-05-22 00:45:08 +0530

    smooth scrolling using js added

--- js/script.js
+++ js/script.js
@@ -11,3 +11,31 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// For Smooth Scrolling Animation
+
+// for selecting more than one element
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    //   to prevent the default scroll
+    e.preventDefault();
+    //   to get the link of the href attribute
+    const href = link.getAttribute("href");
+    // scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    //   Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+  });
+});
