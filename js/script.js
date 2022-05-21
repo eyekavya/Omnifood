907210cf424f5595077cdb96df872c0fcba62291
eyekavya <kavyasrivastava7830@gmail.com>
2022-05-21 23:53:47 +0530

    mobile nav fixed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+// Set current year in footer
+const yearEl = document.querySelector(".year");
+const currenYear = new Date().getFullYear();
+yearEl.textContent = currenYear;
+
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
